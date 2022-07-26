57cdaa2d1db3382c586ef3ef2a85666d8a53aa18
yqchilde <yqchilde@gmail.com>
2022-07-26 23:29:31 +0800

    fix: 优化多选

--- shell/multiSelect.sh
+++ shell/multiSelect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function multiSelect {
-    cursor_blink_on() { printf "\033[?25h"; }                      # 显示光标
-    cursor_blink_off() { printf "\033[?25l"; }                     # 隐藏光标
-    cursor_to() { printf "\033[%s;${2:-1}H" "$1"; }                # 光标移动到指定行位置
+    cursor_blink_on() { printf "\033[?25h"; }                    # 显示光标
+    cursor_blink_off() { printf "\033[?25l"; }                   # 隐藏光标
+    cursor_to() { printf "\033[%s;${2:-1}H" "$1"; }              # 光标移动到指定行位置
     print_active() { printf "%b\033[7m %b \033[27m" "$1" "$2"; } # 打印激活的选项
     print_inactive() { printf "%b %b " "$1" "$2"; }              # 打印未激活的选项
 
@@ -13,22 +13,15 @@ function multiSelect {
         echo "${ROW#*[}"
     }
 
-    local return_value=$1
-    local -n options=$2
-    local -n defaults=$3
-    local selected=()
+    declare -n options=$1
+    declare -A selected
 
-    for ((i = 0; i < ${#options[@]}; i++)); do
-        if [[ ${defaults[i]} = "true" ]]; then
-            selected+=("true")
-        else
-            selected+=("false")
-        fi
+    # 标记选项状态
+    local idx=$((${#options[@]} - 1))
+    for key in "${!options[@]}"; do
+        selected[$idx]="$key"
         printf "\n"
-    done
-
-    for f in "${selected[@]}"; do
-        echo "$f"
+        ((idx--))
     done
 
     # 确定当前屏幕位置以覆盖选项
@@ -36,8 +29,7 @@ function multiSelect {
     local last_row=${cursor_now_row}
     local start_row=$((last_row - ${#options[@]}))
 
-    # 确保在读取 -s 期间在 ctrl+c 上回显光标和输入
-    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    # 确保在read -s期间在ctrl+c上回显光标和输入
     trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
     cursor_blink_off
 
@@ -49,35 +41,37 @@ function multiSelect {
         if [[ $key = $'\x20' ]]; then echo space; fi
         if [[ $key = $'\x1b' ]]; then
             read -rsn2 key
-            if [[ $key = [A || $key = k ]]; then echo up; fi
-            if [[ $key = [B || $key = j ]]; then echo down; fi
+            if [[ $key = [A ]]; then echo up; fi
+            if [[ $key = [B ]]; then echo down; fi
         fi
     }
 
     # 设置选项选中状态
     toggle_option() {
-        local option=$1
-        if [[ ${selected[option]} == true ]]; then
-            selected[option]=false
+        local idx=$1
+        if [[ ${options["${selected[$idx]}"]} = true ]]; then
+            options["${selected[$idx]}"]=false
         else
-            selected[option]=true
+            options["${selected[$idx]}"]=true
         fi
+
+        #        printf "yubo: %s" "${options["${selected[$idx]}"]}"
     }
 
     # 打印勾选状态选项
     print_options() {
         local idx=0
-        for option in "${options[@]}"; do
+        for ((i = 0; i < ${#options[@]}; i++)); do
             local prefix="[ ]"
-            if [[ ${selected[idx]} == true ]]; then
+            if [[ ${options["${selected[$idx]}"]} = true ]]; then
                 prefix="[\e[38;5;46m✔\e[0m]"
             fi
 
             cursor_to $((start_row + idx))
             if [ $idx -eq "$1" ]; then
-                print_active "$prefix" "$option"
+                print_active "$prefix" "${selected[$idx]}"
             else
-                print_inactive "$prefix" "$option"
+                print_inactive "$prefix" "${selected[$idx]}"
             fi
             ((idx++))
         done
@@ -108,17 +102,4 @@ function multiSelect {
     cursor_to "$last_row"
     printf "\n"
     cursor_blink_on
-
-    eval "$return_value"='("${selected[@]}")'
 }
-
-#my_options=("Option 1" "Option 2" "Option 3")
-#preselection=("true" "true" "false")
-#
-#multiSelect result my_options preselection
-#
-#idx=0
-#for option in "${my_options[@]}"; do
-#    echo -e "$option\t=> ${result[idx]}"
-#    ((idx++))
-#done
