a2570db257cce127250491bc3414af4767442d71
yqchilde <yqchilde@gmail.com>
2022-08-26 23:19:23 +0800

    fix: 修复路径

--- shell/githubProxy.sh
+++ shell/githubProxy.sh
@@ -4,13 +4,13 @@
 # ./run.sh stop 关闭代理
 function githubProxy() {
     if [[ "$1" = "start" ]]; then
-        sudo /usr/bin/fastgithub start
+        sudo /usr/bin/fastGithub start
         export http_proxy=http://127.0.0.1:38457
         export https_proxy=http://127.0.0.1:38457
         export socks_proxy=http://127.0.0.1:38457
         echo "github proxy 代理开启，端口：38457"
     elif [[ "$1" = "stop" ]]; then
-        sudo /usr/bin/fastgithub stop
+        sudo /usr/bin/fastGithub stop
         unset http_proxy
         unset https_proxy
         unset socks_proxy
